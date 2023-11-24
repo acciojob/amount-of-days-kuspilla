10ddb8e38572cdba52c97be490070b0bd2f51fb9
Dasthu <118829965+kuspilla@users.noreply.github.com>
2023-11-24 08:34:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function daysOfAYear(year) {
+   if( year % 400  == 0) {
+	   return 366;
+   }
+	   else if(year % 4 == 0 && year % 100 != 0){
+		   return 366;
+	   }
+	else {
+		return 365;
+	}
+}
+
+const temp = prompt("enter year");
+
+   alert( daysOfAYear(temp) );
